9743b56a1bc27ad03cd2f50ed0089baec2230c11
Sebastian Gonzalez <smgonalez@gmail.com>
2025-02-26 15:37:27 -0500

    javascript aprendizaje

---
+++ assets/js/alerts.js
@@ -0,0 +1,14 @@
+
+
+
+alert("hola mundo");
+// 
+// 
+let nombre = prompt("¿Cual es tu nombre?", 'sin nombre');
+console.log("****" + nombre + '****');  // muestra el nombre en la consola
+// 
+// 
+const seleccion = confirm("¿Estas seguro de borrar esto?");
+console.log(seleccion);  // muestra true o false en la consola
+// 
+// console.log(global);
