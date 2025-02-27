2addc2782cc58a61def7cc261108b09f23666f23
Sebastian Gonzalez <smgonalez@gmail.com>
2025-02-26 22:44:42 -0500

    datos primitivos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,29 @@
+
+let nombre = 'peter parker';
+console.log(nombre);
+
+nombre = 'ben parker';
+console.log(nombre);
+
+nombre = "tia may";
+
+console.log(typeof nombre);
+
+let esMarvel = true;
+console.log(typeof esMarvel);
+
+let edad = 33; // number y sigue siendo numero asi sea 3.000033
+console.log(typeof edad);
+
+let superPoder; // undefined
+console.log(typeof superPoder);
+
+let soyNull = null; // object
+console.log(typeof soyNull);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(typeof symbol1);
+
+console.log(symbol1 === symbol2);
