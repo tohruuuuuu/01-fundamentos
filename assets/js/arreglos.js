318b99b09b2ad32bb895f18d50797ffa7ea657eb
Sebastian Gonzalez <smgonalez@gmail.com>
2025-03-10 17:23:39 -0500

    arreglos pt1

---
+++ assets/js/arreglos.js
@@ -0,0 +1,26 @@
+// const arr = new Array(10)
+// const arr = []
+// console.log(arr);
+
+let videojuegos = ['mario 3', 'megaman', 'chronno trigger']
+console.log(videojuegos);
+
+
+console.log( videojuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'sebastian',
+    2 - 1,
+    function(){},
+    ()=>{},
+    { a: 1},
+    ['x', 'megaman', 'zero', 'dr. light', [
+        'woodman', 'drwilly'
+    ]]
+];
+// console.log(arregloCosas);
+
+console.log(arregloCosas[7][3]);// pa imprimir dr.light
+
