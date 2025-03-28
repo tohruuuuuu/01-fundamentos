78b928dbde7f80077b9c12b5708e9cae165496e5
Sebastian Gonzalez <smgonalez@gmail.com>
2025-03-28 18:27:59 -0500

    mas sobre los arreglos

---
+++ assets/js/arreglos2.js
@@ -0,0 +1,35 @@
+let juegos = ['zelda', 'mario', 'metroid', 'chrono']
+console.log('largo',  juegos.length)
+
+let primero = juegos[0];
+let ultimo = juegos[juegos.length - 1 ];
+
+console.log(primero);
+console.log(ultimo);
+
+juegos.forEach(( Element, index, arr )  => {
+    console.log({Element, index, arr});
+});
+
+let nuevaLongitud = juegos.push('f-zero'); 
+console.log({nuevaLongitud, juegos});
+
+nuevaLongitud = juegos.unshift('fire emblem');
+console.log({nuevaLongitud, juegos});
+
+let juegoBorrado = juegos.pop();
+console.log({juegoBorrado, juegos});
+
+let pos = 1;
+
+let juegosBorrado = juegos.splice(pos, 2);
+console.log({ juegosBorrado, juegos});
+
+let metroidIndex = juegos.indexOf('metroid');//key sensitive
+console.log({metroidIndex});
+
+
+
+
+
+
