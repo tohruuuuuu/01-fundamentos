122efe85014b4c9e4d2e15fdd53cf21f732389b0
Sebastian Gonzalez <smgonalez@gmail.com>
2025-04-02 15:37:24 +0200

    objetos literales pt2

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -1,5 +1,5 @@
 
-let personaje = {
+const personaje = {
     nombre: 'Tony Stark',
     codeName: 'Ironman',
     vivo: false,
@@ -29,3 +29,18 @@ console.log('ultimo traje', personaje.trajes[personaje.trajes.length - 1])
 const x = 'vivo';
 console.log('vivo', personaje[x]);
 
+//details
+
+delete personaje.edad;
+console.log(personaje);
+
+personaje.casado = true
+
+const entriesPares = Object.entries(personaje);
+console.log(entriesPares);
+
+ Object.freeze (personaje);
+
+ personaje.dinero = 10000000; // no sale por lo del .freeze, pero si 
+ // se pueden cambiar las propiedades dentro del objeto
+ console.log(personaje)
