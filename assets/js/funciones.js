9c7078abb896631bd04b84c52a385b2118ea2e9c
Sebastian Gonzalez <smgonalez@gmail.com>
2025-04-04 20:05:56 +0200

    funciones

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,20 +1,54 @@
-// function saludar() {
-//     console.log('hola mundo');
-// }
+function saludar() {
+    console.log('hola mundo');
+    return 1;
+
+    console.log('esto es codigo despues de un return')// esto nunca se va a ejecurar
+}
 
 
-const saludar = function(nombre ) {
+const saludar2 = function(nombre ) {
 console.log('hola ' + nombre);
 }// esto es una funcion anonima
 //sirve para q no se pueda usar el nombre de la variable 
 // dos veces
 
 
-saludar('sebastian');
 
 
-const saludarFlecha = (nombreFlecha) => {
+const saludarFlecha = () => {
+    console.log('hola ');
+}
+
+const saludarFlecha2 = (nombreFlecha) => {
     console.log('hola ' + nombreFlecha);
 }
 
-saludarFlecha("flechaVeloz");
+
+const retornoSaludar = saludar;
+console.log(retornoSaludar)
+
+
+saludar();
+saludar2('sebastian');
+saludarFlecha();
+saludarFlecha2("flechaVeloz");
+
+
+function sumar( a, b ) {
+    return a + b;
+}
+
+const sumar2 = (a,b) => a + b;// funciona igual al de arriba
+
+
+function getAleatorio() {
+    return Math.random();
+}
+
+
+const getAleatorio2 = () => Math.random();
+
+console.log( sumar(2,3));
+console.log( sumar2(2,3));
+console.log( getAleatorio());
+console.log( getAleatorio2());
