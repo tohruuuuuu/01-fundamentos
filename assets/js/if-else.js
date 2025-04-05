de781e0abf3a1a33b4899da4da03192cdf3052e5
Sebastian Gonzalez <smgonalez@gmail.com>
2025-04-05 18:05:34 +0200

    if-else

---
+++ assets/js/if-else.js
@@ -0,0 +1,26 @@
+
+
+let a = 5;
+
+
+if ( a > 10 ) {// undefined, null, asignaciones
+    console.log('a es mayor a 10');
+} else {
+    console.log( 'a es menor a 10');
+}
+
+
+// console.log('fin del programa');
+
+const hoy = new Date();
+let dia = hoy.getDay();// 0: domingo, 1: lunes, 2: martes etc
+
+console.log({dia});
+
+if ( dia === 0 ) {
+    console.log( 'hoy es domingo');
+} else {
+    console.log('NO ES DOMINGOOO');
+}
+
+ 
