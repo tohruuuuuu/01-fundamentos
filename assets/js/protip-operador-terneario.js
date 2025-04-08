9c5888371340f40da424571714df032e6b419d5f
Sebastian Gonzalez <smgonalez@gmail.com>
2025-04-08 09:46:43 +0200

    switch

--- assets/js/protip-operador-terneario.js
+++ assets/js/protip-operador-terneario.js
@@ -50,3 +50,5 @@ console.log({nota, grado});
 
 
 
+
+
---
+++ assets/js/switch.js
@@ -0,0 +1,30 @@
+
+
+
+const dia = 0;
+
+switch( dia ) {
+    case 0:
+        console.log('domingo');
+        break;
+    case 1:
+        console.log('lunes');
+        break;
+    case 2:
+        console.log('martes');
+        break;
+    case 3:
+        console.log('miercoles');
+        break;
+    case 4:
+        console.log('jueves');
+        break;
+    case 5:
+        console.log('viernes');
+        break;
+    case 6:
+        console.log('sabado');
+        break;
+    default: 
+        console.log('respuesta cuando no hay casos')
+}
